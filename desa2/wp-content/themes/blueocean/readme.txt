9f1d13a650243242b86c9c4b2866f3291a3b59af
Fede Mac <fedomkf1@gmail.com>
2020-03-31 02:03:24 -0300

    borrando desas

--- desa2/wp-content/themes/blueocean/readme.txt
+++
@@ -1,4 +0,0 @@
-Theme Author: http://smthemes.com
-Theme Homepage: http://smthemes.com/blueocean/
-Buy theme: http://smthemes.com/buy/blueocean/
-Support Forums: http://smthemes.com/support/forum/blueocean-free-wordpress-theme/
--- desa2/wp-content/themes/invert-lite/searchform.php
+++
@@ -1,9 +0,0 @@
-<form method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>">
-	<div class="searchleft">
-		<input type="text" value="" name="s" id="searchbox" placeholder="Search.." class="searchinput"/>
-	</div>
-	<div class="searchright">
-		<input type="submit" class="submitbutton" value="Search" />
-	</div>
-	<div class="clear"></div>
-</form>
